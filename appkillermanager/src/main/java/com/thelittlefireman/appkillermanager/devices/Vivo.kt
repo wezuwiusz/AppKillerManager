1d9231626a7caaaa67abba342842fbd24af51064
Mikołaj Pich <m.pich@outlook.com>
2020-05-17 15:44:55 +0200

    Convert Vivo to kotlin

---
+++ appkillermanager/src/main/java/com/thelittlefireman/appkillermanager/devices/Vivo.kt
@@ -0,0 +1,50 @@
+package com.thelittlefireman.appkillermanager.devices
+
+import android.content.Context
+import com.thelittlefireman.appkillermanager.utils.Manufacturer
+
+class Vivo : Device() {
+
+    // TODO multiple intent in a same actions !
+    // Starting: Intent { cmp=com.vivo.permissionmanager/.activity.BgStartUpManagerActivity }
+    //java.lang.SecurityException: Permission Denial: starting Intent { flg=0x10000000 cmp=com.vivo.permissionmanager/.activity.BgStartUpManagerActivity } from null (pid=28141, uid=2000) not exported from uid 1000
+    private val p1 = "com.iqoo.secure"
+    private val p1c1 = "com.iqoo.secure.ui.phoneoptimize.AddWhiteListActivity"
+    private val p1c2 = "com.iqoo.secure.ui.phoneoptimize.BgStartUpManager"
+    private val p2 = "com.vivo.permissionmanager"
+    private val p2c1 = "com.vivo.permissionmanager.activity.BgStartUpManagerActivity"
+
+    // "com.vivo.abe", "com.vivo.applicationbehaviorengine.ui.ExcessivePowerManagerActivity"
+    //com.iqoo.secure.MainGuideActivity ??
+    override fun isThatRom() = false
+
+    override fun getDeviceManufacturer() = Manufacturer.VIVO
+
+    override fun isActionPowerSavingAvailable(context: Context) = false
+
+    override fun isActionAutoStartAvailable(context: Context) = false
+
+    override fun isActionNotificationAvailable(context: Context) = false
+
+    override fun getActionPowerSaving(context: Context) = null
+
+    override fun getActionAutoStart(context: Context) = null
+
+    override fun getActionNotification(context: Context) = null
+
+    override fun getExtraDebugInformations(context: Context) = null
+
+    /*
+       @Override
+       public List<ComponentName> getAutoStartSettings(Context context) {
+           List<ComponentName> componentNames = new ArrayList<>();
+           if(ActionsUtils.isPackageExist(context, p1)){
+               componentNames.add(new ComponentName(p1,p1c1));
+               componentNames.add(new ComponentName(p1,p1c2));
+           }
+           if(ActionsUtils.isPackageExist(context,p2)){
+               componentNames.add(new ComponentName(p2,p2c1));
+           }
+           return componentNames;
+       }*/
+}

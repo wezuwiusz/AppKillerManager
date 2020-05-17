11af4ed7dd995a54336a66080b11e000d1ec43a2
Mikołaj Pich <m.pich@outlook.com>
2020-05-17 15:51:49 +0200

    Convert Oppo to kotlin

---
+++ appkillermanager/src/main/java/com/thelittlefireman/appkillermanager/devices/Oppo.kt
@@ -0,0 +1,63 @@
+package com.thelittlefireman.appkillermanager.devices
+
+import android.content.Context
+import android.os.Build
+import com.thelittlefireman.appkillermanager.utils.Manufacturer
+
+class Oppo : Device() {
+
+    companion object {
+        // TODO multiple intent in a same actions need to be refractor!
+        /** java.lang.SecurityException: Permission Denial: starting Intent { cmp=com.coloros.safecenter/.startupapp.StartupAppListActivity } from ProcessRecord{7eba0ba 27527:crb.call.follow.mycrm/u0a229} (pid=27527, uid=10229) requires oppo.permission.OPPO_COMPONENT_SAFE*/
+        //coloros3.0
+        private const val p1 = "com.coloros.safecenter"
+        private const val p1c1 = "com.coloros.safecenter.permission.startup.StartupAppListActivity"
+        private const val p1c2 = "com.coloros.safecenter.startupapp.StartupAppListActivity"
+        private const val p12 = "com.coloros.oppoguardelf"
+        private const val p12c1 = "com.coloros.powermanager.fuelgaue.PowerConsumptionActivity"
+        private const val p12c2 = "com.coloros.powermanager.fuelgaue.PowerUsageModelActivity"
+
+        //OLD == ColorOS V2.1
+        private const val p13 = "com.color.oppoguardelf"
+        private const val p13c1 = "com.color.safecenter.permission.startup.StartupAppListActivity"
+        private const val p13c2 = "com.color.safecenter.startupapp.StartupAppListActivity"
+        private const val p2 = "com.oppo.safe"
+        private const val p2c1 = "com.oppo.safe.permission.startup.StartupAppListActivity"
+    }
+
+    override fun isThatRom() = Build.BRAND.equals(deviceManufacturer.toString(), ignoreCase = true) ||
+            Build.MANUFACTURER.equals(deviceManufacturer.toString(), ignoreCase = true) ||
+            Build.FINGERPRINT.contains(deviceManufacturer.toString(), ignoreCase = true)
+
+    override fun getDeviceManufacturer() = Manufacturer.OPPO
+
+    override fun isActionPowerSavingAvailable(context: Context) = false
+
+    override fun isActionAutoStartAvailable(context: Context) = false
+
+    override fun isActionNotificationAvailable(context: Context) = false
+
+    override fun getActionPowerSaving(context: Context) = null
+
+    override fun getActionAutoStart(context: Context) = null
+
+    override fun getActionNotification(context: Context) = null
+
+    override fun getExtraDebugInformations(context: Context) = null
+/*
+    private ComponentName getComponentName(Context context){
+        if(ActionsUtils.isPackageExist(context,p1)){
+
+        }
+        else if(ActionsUtils.isPackageExist(context,p12)){
+
+        }
+        else if(ActionsUtils.isPackageExist(context,p13)){
+
+        }
+        else if(ActionsUtils.isPackageExist(context,p2)){
+
+        }
+        return null;
+    }*/
+}
